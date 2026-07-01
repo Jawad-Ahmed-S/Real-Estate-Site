6ec1732e2118f62d3c10ca5be33958498c581149
soomrojawad1.2.2006@gmail.com <soomrojawad1.2.2006@gmail.com>
2026-07-01 18:56:10 +0500

    connected db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv, { configDotenv } from "dotenv"
+import mongoose from "mongoose";
 
 const app = express()
+configDotenv()
 
+mongoose.connect(process.env.MONGODB_URI)
 
 app.listen(8000,()=>{console.log("Server Started!")})
