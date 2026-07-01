af68fe1e587d2a9047fab3b9ed633c1290903c39
soomrojawad1.2.2006@gmail.com <soomrojawad1.2.2006@gmail.com>
2026-07-01 18:43:13 +0500

    created pages and routes and server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     App
+     
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/login' element={<Signin/>}></Route>
+            <Route path='/signin' element={<Login/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
+            <Route path='/profile' element={<Profile/>}></Route>
+        </Routes>
+        </BrowserRouter>
     </>
   )
 }
